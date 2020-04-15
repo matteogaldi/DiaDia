a97c1bddf8f0c6c2e74ba5275742a1d9584ea3e3
Matteo Galdi <matteo1.galdi@gmail.com>
2020-04-15 10:58:19 +0200

    modifiche su partita

--- diadia/src/it/uniroma3/diadia/Partita.java
+++ diadia/src/it/uniroma3/diadia/Partita.java
@@ -2,7 +2,6 @@ package it.uniroma3.diadia;
 
 import it.uniroma3.diadia.ambienti.Labirinto;
 import it.uniroma3.diadia.ambienti.Stanza;
-import it.uniroma3.diadia.attrezzi.Attrezzo;
 import it.uniroma3.diadia.giocatore.Giocatore;
 
 /**
