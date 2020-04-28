80a04c4099734ecf1068f2a5c22add4ab84b9ace
Matteo Galdi <galdi.m@protonmail.com>
2020-04-28 12:13:39 +0200

    - modifica DiaDia per implementare la nuova interface IO

--- diadia/src/it/uniroma3/diadia/DiaDia.java
+++ diadia/src/it/uniroma3/diadia/DiaDia.java
@@ -1,7 +1,5 @@
 package it.uniroma3.diadia;
 
-import it.uniroma3.diadia.ambienti.Stanza;
-import it.uniroma3.diadia.attrezzi.Attrezzo;
 import it.uniroma3.diadia.comandi.Comando;
 import it.uniroma3.diadia.comandi.FabbricaDiComandi;
 import it.uniroma3.diadia.comandi.FabbricaDiComandiFisarmonica;
@@ -12,69 +10,68 @@ import it.uniroma3.diadia.comandi.FabbricaDiComandiFisarmonica;
  * <p>
  * Questa e' la classe principale crea e istanzia tutte le altre
  *
- * @author docente di POO
- * (da un'idea di Michael Kolling and David J. Barnes)
+ * @author docente di POO (da un'idea di Michael Kolling and David J. Barnes)
  * @version base
  */
 
 public class DiaDia {
 
-	static final private String MESSAGGIO_BENVENUTO = "" +
-			"Ti trovi nell'Universita', ma oggi e' diversa dal solito...\n" +
-			"Meglio andare al piu' presto in biblioteca a studiare. Ma dov'e'?\n" +
-			"I locali sono popolati da strani personaggi, " +
-			"alcuni amici, altri... chissa!\n" +
-			"Ci sono attrezzi che potrebbero servirti nell'impresa:\n" +
-			"puoi raccoglierli, usarli, posarli quando ti sembrano inutili\n" +
-			"o regalarli se pensi che possano ingraziarti qualcuno.\n\n" +
-			"Per conoscere le istruzioni usa il comando 'aiuto'.";
-
+	static final private String MESSAGGIO_BENVENUTO = ""
+			+ "Ti trovi nell'Universita', ma oggi e' diversa dal solito...\n"
+			+ "Meglio andare al piu' presto in biblioteca a studiare. Ma dov'e'?\n"
+			+ "I locali sono popolati da strani personaggi, " + "alcuni amici, altri... chissa!\n"
+			+ "Ci sono attrezzi che potrebbero servirti nell'impresa:\n"
+			+ "puoi raccoglierli, usarli, posarli quando ti sembrano inutili\n"
+			+ "o regalarli se pensi che possano ingraziarti qualcuno.\n\n"
+			+ "Per conoscere le istruzioni usa il comando 'aiuto'.";
 
 	private Partita partita;
-	public static IOConsole iO;
+	private IO iO;
 
-	public DiaDia() {
+	public DiaDia(IO io) {
 		this.partita = new Partita();
+		this.iO = io;
 	}
 
 	public void gioca() {
 		String istruzione;
 
 		iO.mostraMessaggio(MESSAGGIO_BENVENUTO);
-		//scannerDiLinee = new Scanner(System.in);
+		// scannerDiLinee = new Scanner(System.in);
 		do
 			istruzione = iO.leggiRiga();
 		while (!processaIstruzione(istruzione));
 	}
 
-
 	/**
 	 * Processa una istruzione
 	 *
-	 * @return true se l'istruzione e' eseguita e il gioco continua, false altrimenti
+	 * @return true se l'istruzione e' eseguita e il gioco continua, false
+	 *         altrimenti
 	 */
 	private boolean processaIstruzione(String istruzione) {
 		Comando comandoDaEseguire;
 		FabbricaDiComandi factory = new FabbricaDiComandiFisarmonica();
 
 		comandoDaEseguire = factory.costruisciComando(istruzione);
+		comandoDaEseguire.setIO(iO);
 		comandoDaEseguire.esegui(this.partita);
 
 		if (this.partita.getGiocatore().getCfu() == 0) {
 			iO.mostraMessaggio("Game Over! Hai terminato i cfu");
 		}
-		
+
 		if (this.partita.vinta()) {
 			iO.mostraMessaggio("Hai vinto!");
 		}
-		
+
 		return this.partita.isFinita();
 	}
 
-
 	public static void main(String[] argc) {
-		DiaDia gioco = new DiaDia();
-		iO = new IOConsole();
+		IO iO = new IOConsole();
+		DiaDia gioco = new DiaDia(iO);
+
 		gioco.gioca();
 	}
 }
