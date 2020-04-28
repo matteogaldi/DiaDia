74912379ff8479003454701d02b0fc20a3dd01fc
Matteo Galdi <galdi.m@protonmail.com>
2020-04-28 12:14:04 +0200

    - rimuove import non utilizzati

--- diadia/test/it/uniroma3/diadia/PartitaTest.java
+++ diadia/test/it/uniroma3/diadia/PartitaTest.java
@@ -12,18 +12,15 @@ import org.junit.Before;
 public class PartitaTest {
 
 	private Partita partita;
-	private Stanza stanzaCorrente;
 	private Stanza stanzaVincente;
 	private Stanza provaStanza;
 	private Labirinto labirinto;
-
 
 	@Before
 	public void setUp() {
 		this.partita = new Partita();
 		this.labirinto = new Labirinto();
 		this.stanzaVincente = this.labirinto.getStanzaVincente();
-		this.stanzaCorrente = partita.getStanzaCorrente();
 		this.provaStanza = new Stanza("Prova");
 
 	}
@@ -32,31 +29,33 @@ public class PartitaTest {
 	public void testStanzaIniziale() {
 		assertNotNull("Non dovrebbe essere null", partita.getStanzaCorrente());
 	}
+
 	@Test
 	public void testSetStanzaCorrente() {
 		partita.setStanzaCorrente(provaStanza);
 		assertNotNull("Non dovrebbe essere null", partita.getStanzaCorrente());
 	}
-	
+
 	// TEST getStanzaVincente
 	@Test
 	public void testStanzaVincente() {
 		assertEquals("Stanza vincente non coincide con quella fornita da Labirinto ", stanzaVincente.toString(),
 				partita.getStanzaVincente().toString());
 	}
+
 	@Test
 	public void testPartitaVinta() {
 		partita.setStanzaCorrente(stanzaVincente);
 		assertEquals("Dovrebbe aver vinto", stanzaVincente.toString(), partita.getStanzaCorrente().toString());
 	}
 
-
 	// TEST isFinita
-	@Test 
+	@Test
 	public void testParitaFinitaNoCfu() {
 		partita.giocatore.setCfu(0);
 		assertTrue("Dovrebbe essere finita", partita.isFinita());
 	}
+
 	@Test
 	public void testPartitaFinitaSetTrue() {
 		partita.setFinita();
